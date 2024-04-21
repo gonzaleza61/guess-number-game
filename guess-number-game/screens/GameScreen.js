58480f9ddcbeab6008d35ac4a3c655d276db30da
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2024-04-21 11:15:13 -0500

    random number function

--- guess-number-game/screens/GameScreen.js
+++ guess-number-game/screens/GameScreen.js
@@ -2,6 +2,16 @@ import { View, Text, StyleSheet } from "react-native";
 import Title from "../components/Title";
 
 function GameScreen() {
+  function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if (rndNum === exclude) {
+      return generateRandomBetween(min, max, exclude);
+    } else {
+      return rndNum;
+    }
+  }
+
   return (
     <>
       <View style={styles.screen}>

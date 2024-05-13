ff2f55d4f8cb557873037788e3baba65d39280a4
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2024-05-12 23:08:36 -0500

    number container and styling

--- guess-number-game/screens/GameScreen.js
+++ guess-number-game/screens/GameScreen.js
@@ -1,22 +1,28 @@
 import { View, Text, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import Title from "../components/ui/Title";
+import { useState } from "react";
+import NumberContainer from "../components/game/NumberContainer";
 
-function GameScreen() {
-  function generateRandomBetween(min, max, exclude) {
-    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
 
-    if (rndNum === exclude) {
-      return generateRandomBetween(min, max, exclude);
-    } else {
-      return rndNum;
-    }
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
   }
+}
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
 
   return (
     <>
       <View style={styles.screen}>
-        {/*GUESS*/}
+        <Text></Text>
         <Title title="Opponent's Guess" />
+        <NumberContainer>{currentGuess}</NumberContainer>
         <Text>Higher Or Lower</Text>
         {/*+*/}
         {/*-*/}
